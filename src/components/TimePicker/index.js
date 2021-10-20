55643fd0ea4c9b6c750dd94a10fc9b91ab26ebe2
alexted4 <79405751+alexted4@users.noreply.github.com>
2021-10-20 13:56:06 +0700

    fix: got rid of NaN in input

--- src/components/TimePicker/index.js
+++ src/components/TimePicker/index.js
@@ -3,13 +3,21 @@ import useStyles from './style';
 
 const TimePicker = ({ time, setTime }) => {
 	const classes = useStyles();
+
+	const handleTimeChange = (e) => {
+		const value = e.target.value;
+		return Number.isNaN(parseInt(value))
+			? alert('Time must be a number')
+			: setTime(parseInt(value));
+	};
+
 	return (
 		<div className={classes.root}>
 			<InputLabel shrink>Minutes to complete</InputLabel>
 			<Input
 				className={classes.dateInput}
 				value={time}
-				onChange={(e) => setTime(parseInt(e.target.value))}
+				onChange={(e) => handleTimeChange(e)}
 			/>
 		</div>
 	);
